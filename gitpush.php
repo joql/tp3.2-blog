686191e2bdbf7d302c6b28cf7b2690e47bce31c9
Joql <ghjoql001>
2017-10-26 23:34:58 +0800

    init

---
+++ gitpush.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User:ksj
+ * Date: 2017/10/16
+ * Time: 09:45
+ */
+
+if($name = $_GET['name']){
+    exec("cd /home/wwwroot/".$name."/ && sudo git pull 2>&1");
+    $str = file_get_contents("php://input");
+    $tmp = json_decode($str,true);
+    echo $tmp['head_commit']['message'].'提交成功';
+}else{
+    die('error');
+}
